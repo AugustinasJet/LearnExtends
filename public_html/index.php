a7b32b85c0f249ccd20540b1d12f7e00a9ce8166
Augustinas <augustinasciuta@gmail.com>
2019-04-02 11:28:29 +0100

    5dienio projektas klasej

--- public_html/index.php
+++ public_html/index.php
@@ -1,32 +1,71 @@
 <?php
+
 class Girl {
-    public function beSmart(){
+
+    private $age;
+    protected $complexion;
+
+    public function __construct($age, $complexion) {
+        $this->age = $age;
+        $this->complexion = $complexion;
+    }
+
+    public function getAge() {
+        return $this->age;
+    }
+
+    public function beSmart() {
         return "I'm smart";
     }
+
     public function beBeautiful() {
         return "I'm beautiful";
     }
-    
+
 }
 
 class Girlfriend extends Girl {
+
     public function pistiProta() {
-        return "ka ten palaikinai fb?";
+        return " ka ten palaikinai fb?";
     }
+
 }
 
 class Wife extends Girlfriend {
+
     public function pistiProta() {
         return "baigsi gert?";
     }
+
 }
 
 class HorribleWife extends Girlfriend {
-    public function pistiProta(){
-        return parent::pistiProta(). ' ' . 'štuka eurū arba nematai vaikū';
+
+    public function pistiProta() {
+        return parent::pistiProta() . ' ' . 'štuka eurū arba nematai vaikū ' . $this->complexion;
+    }
+
+}
+
+class Boy {
+    public function winArgument(){
+        return 'You\'ve won';
     }
 }
-$girl = new HorribleWife;
+
+class Boyfriend extends Boy {
+    public function winArgument(){
+        if(rand(0, 1)){
+            return parent::winArgument();
+        }
+        return 'Not this time';
+    }
+}
+$girl = new HorribleWife(21, 'black');
 $message = $girl->pistiProta();
+print 'Amzius: ' . $girl->getAge();
 print $message;
+$boy= new Boyfriend();
+print $boy->winArgument();
 ?>
